7abedfb1b3170d972792ec78d397e2d2bca2660a
Ruk Doe <info@doany.io>
2024-04-09 22:02:16 +0900

    fix

--- cu.ts
+++ cu.ts
@@ -1,70 +1,95 @@
-import { load } from 'cheerio'
-import { writeFileSync } from 'fs'
-const url = 'https://tabelog.com/rstLst'
-let ratio = {}
+import { load } from "cheerio";
+
+const url = "https://tabelog.com/rstLst";
+let ratio = {};
 let plist = {
-    all: {},
-    card: {}
-}
+  all: {},
+  card: {},
+};
 
 Promise.all([
-    fetch(url).then((data) => data.text()).then(async (text) => {
-        let $ = load(text);
-        if(ratio['全国']) {
-            ratio['全国'].all = $('.c-page-count>.c-page-count__num:last-of-type>strong').text()
-        } else {
-            ratio['全国'] = {all: $('.c-page-count>.c-page-count__num:last-of-type>strong').text()}
-        }
-        $('.list-balloon__table--pref>dd>ul>li>a').each((index, element) => {
-            let pre = element.children[0].data.trim();
-            plist.all[pre] = element.attribs.href
+  fetch(url)
+    .then((data) => data.text())
+    .then(async (text) => {
+      let $ = load(text);
+      if (ratio["全国"]) {
+        ratio["全国"].all = $(
+          ".c-page-count>.c-page-count__num:last-of-type>strong"
+        ).text();
+      } else {
+        ratio["全国"] = {
+          all: $(".c-page-count>.c-page-count__num:last-of-type>strong").text(),
+        };
+      }
+      $(".list-balloon__table--pref>dd>ul>li>a").each((_index, element) => {
+        const che = $(element);
+        let pre = che.first().text().trim();
+        plist.all[pre] = che.attr("href");
+      });
+      await Promise.all(
+        Object.keys(plist.all).map(async (pref) => {
+          const data = await fetch(plist.all[pref]);
+          const text = await data.text();
+          let $ = load(text);
+          if (ratio[pref]) {
+            ratio[pref].all = $(
+              ".c-page-count>.c-page-count__num:last-of-type>strong"
+            ).text();
+          } else {
+            ratio[pref] = {
+              all: $(
+                ".c-page-count>.c-page-count__num:last-of-type>strong"
+              ).text(),
+            };
+          }
         })
-        await Promise.all(
-            Object.keys(plist.all).map(pref => {
-                return fetch(plist.all[pref]).then((data) => data.text()).then((text) => {
-                    let $ = load(text)
-                    if(ratio[pref]) {
-                        ratio[pref].all = $('.c-page-count>.c-page-count__num:last-of-type>strong').text()
-                    } else {
-                        ratio[pref] = {all: $('.c-page-count>.c-page-count__num:last-of-type>strong').text()}
-                    }
-                })
-            })
-        )
+      );
     }),
-    fetch(url+'?ChkCard=1').then((data) => data.text()).then(async (text) => {
-        let $ = load(text);
-        if(ratio['全国']) {
-            ratio['全国'].card = $('.c-page-count>.c-page-count__num:last-of-type>strong').text()
-        } else {
-            ratio['全国'] = {card: $('.c-page-count>.c-page-count__num:last-of-type>strong').text()}
-        }
-        $('.list-balloon__table--pref>dd>ul>li>a').each((index, element) => {
-            let pre = element.children[0].data.trim();
-            plist.card[pre] = element.attribs.href
-            fetch(element.attribs.href).then((data) => data.text()).then((text) => {
-                let $ = load(text);
-            })
+  fetch(url + "?ChkCard=1")
+    .then((data) => data.text())
+    .then(async (text) => {
+      let $ = load(text);
+      if (ratio["全国"]) {
+        ratio["全国"].card = $(
+          ".c-page-count>.c-page-count__num:last-of-type>strong"
+        ).text();
+      } else {
+        ratio["全国"] = {
+          card: $(
+            ".c-page-count>.c-page-count__num:last-of-type>strong"
+          ).text(),
+        };
+      }
+      $(".list-balloon__table--pref>dd>ul>li>a").each((_index, element) => {
+        const che = $(element);
+        let pre = che.first().text().trim();
+        plist.card[pre] = che.attr("href");
+      });
+      await Promise.all(
+        Object.keys(plist.card).map(async (pref) => {
+          const data = await fetch(plist.card[pref]);
+          const text = await data.text();
+          let $ = load(text);
+          if (ratio[pref]) {
+            ratio[pref].card = $(
+              ".c-page-count>.c-page-count__num:last-of-type>strong"
+            ).text();
+          } else {
+            ratio[pref] = {
+              card: $(
+                ".c-page-count>.c-page-count__num:last-of-type>strong"
+              ).text(),
+            };
+          }
         })
-        await Promise.all(
-            Object.keys(plist.card).map(pref => {
-                return fetch(plist.card[pref]).then((data) => data.text()).then((text) => {
-                    let $ = load(text)
-                    if(ratio[pref]) {
-                        ratio[pref].card = $('.c-page-count>.c-page-count__num:last-of-type>strong').text()
-                    } else {
-                        ratio[pref] = {card: $('.c-page-count>.c-page-count__num:last-of-type>strong').text()}
-                    }
-                })
-            })
-        )
-    })
+      );
+    }),
 ]).finally(() => {
-    let data = {
-        '全国': ratio['全国']
-    }
-    Object.keys(plist.all).map(pref => {
-        data[pref] = ratio[pref]
-    })
-    writeFileSync('./ratio.json', JSON.stringify(data, null, 4))
-})
+  let data = {
+    全国: ratio["全国"],
+  };
+  Object.keys(plist.all).map((pref) => {
+    data[pref] = ratio[pref];
+  });
+  Bun.write("./ratio.json", JSON.stringify(data, null, 4));
+});
